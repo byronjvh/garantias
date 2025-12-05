673d95e8667f34a364864cb1545568b40fe0859c
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-05 16:35:32 -0600

    add: Loading effect

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -1,10 +1,23 @@
+import styles from "./PrimaryButton.module.css"
+
 interface Props {
     text: string,
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
+    onClick?: React.MouseEventHandler<HTMLButtonElement>,
+    loading?: boolean
 }
 
-export default function PrimaryButton({ text, onClick }: Props) {
+export default function PrimaryButton({ text, onClick, loading }: Props) {
     return (
-        <button onClick={onClick} className="bg-accent cursor-pointer px-3 py-2 text-sm font-bold text-white hover:brightness-110 ease-out transition-all duration-200 rounded">{text}</button>
+        <button onClick={onClick} className="bg-accent cursor-pointer px-4 py-2 text-sm font-bold text-white hover:brightness-110 ease-out transition-all duration-200 rounded relative">
+            <p className={`inline ${!loading ? "opacity-0" : "opacity-100"}`}>{text}</p>
+            <p className={`loading-effect flex flex-nowrap absolute left-1/2 top-1/2 -translate-1/2 ${!loading ? "inline" : "hidden"}`}>
+                Cargando
+                <div className="dots ml-0.5 inline">
+                    <span className="dot-1">.</span>
+                    <span className="dot-2">.</span>
+                    <span className="dot-3">.</span>
+                </div>
+            </p>
+        </button>
     )
 }
