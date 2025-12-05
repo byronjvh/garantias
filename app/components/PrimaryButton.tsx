70affcbcc5a7128b45d4fd626e09c586df2b7706
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-05 16:54:52 -0600

    fix: Reverse the loading order

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -9,8 +9,8 @@ interface Props {
 export default function PrimaryButton({ text, onClick, loading }: Props) {
     return (
         <button onClick={onClick} className="bg-accent cursor-pointer px-4 py-2 text-sm font-bold text-white hover:brightness-110 ease-out transition-all duration-200 rounded relative">
-            <p className={`inline ${!loading ? "opacity-0" : "opacity-100"}`}>{text}</p>
-            <p className={`loading-effect flex flex-nowrap absolute left-1/2 top-1/2 -translate-1/2 ${!loading ? "inline" : "hidden"}`}>
+            <p className={`inline ${loading ? "opacity-0" : "opacity-100"}`}>{text}</p>
+            <p className={`loading-effect flex flex-nowrap absolute left-1/2 top-1/2 -translate-1/2 ${loading ? "inline" : "hidden"}`}>
                 Cargando
                 <div className="dots ml-0.5 inline">
                     <span className="dot-1">.</span>
