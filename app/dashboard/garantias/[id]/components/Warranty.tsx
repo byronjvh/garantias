63c505f8d1272dd5c5ae2726164cc633cae57ba5
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-09 11:10:53 -0600

    UPD: warranty pdf styles

--- app/dashboard/garantias/[id]/components/Warranty.tsx
+++ app/dashboard/garantias/[id]/components/Warranty.tsx
@@ -9,79 +9,71 @@ export default function Warranty({ id, warranty }: Props) {
 
     return (
         <div id="pdf" className="h-[11in] w-[8.5in] bg-white flex flex-col gap-4 p-10 text-xs">
-            <section className="">
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Fecha:</p> <p className="value">{warranty?.fecha}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Número Telefónico:</p> <p className="value">{warranty?.telefono}</p></div>
-                </div>
-                <div className="flex row"><p className="label">Nombre del cliente:</p> <p className="value name">{warranty?.nombre}</p></div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Factura:</p> <p className="value">{warranty?.factura}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Correo:</p> <p className="value">{warranty?.correo}</p></div>
-                </div>
-            </section>
-            <section>
-                <div className="flex-1 flex row"><p className="label">Descripcion del Equipo:</p> <p className="value">{warranty?.producto.descripcion}</p></div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">SKU:</p> <p className="value">{warranty?.producto.sku}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Equipo enciende:</p> <p className="value">{warranty?.producto.enciende ? "Sí" : "No"}</p></div>
-                </div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Contraseña:</p> <p className="value">{warranty?.producto.contrasena}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Se puede formatear:</p> <p className="value">{warranty?.producto.puede_formatear ? "Sí" : "No"}</p></div>
+            <section className="header flex overflow-hidden justify-between items-center">
+                <img src="/header.webp" className="h-full w-[350px] object-cover object-right" alt=" " />
+                <div className="py-4 pr-10">
+                    <h3 className="font-bold text-lg">Orden de Servicio #{id}</h3>
+                    <p>
+                        Correo: soporte.yoses@crtechstore.com <br />
+                        Teléfono CR TECH: 2234-1032 <br />
+                        TUKOMER S.R.L <br />
+                        Ced.Juridica: 3-102-870210
+                    </p>
                 </div>
             </section>
-            <section>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Procesador:</p> <p className="value">{warranty?.producto.cpu}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Tarjeta de video:</p> <p className="value">{warranty?.producto.gpu}</p></div>
-                </div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Memoria RAM:</p> <p className="value">{warranty?.producto.ram.cantidad}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Módulos:</p> <p className="value">{warranty?.producto.ram.modulos}</p></div>
-                </div>
+            <section className="warranty-section">
+                <p className="label col-span-3">Fecha:</p> <p className="value col-span-3">{warranty?.fecha}</p>
+                <p className="label col-span-3">Número Telefónico:</p> <p className="value col-span-3">{warranty?.telefono}</p>
+                <p className="label col-span-4">Nombre del cliente:</p> <p className="value name col-span-8">{warranty?.nombre}</p>
+                <p className="label col-span-3">Factura:</p> <p className="value col-span-3">{warranty?.factura}</p>
+                <p className="label col-span-3">Correo:</p> <p className="value col-span-3">{warranty?.correo}</p>
+            </section>
+            <section className="warranty-section">
+                <p className="label col-span-4">Descripcion del Equipo:</p> <p className="value col-span-8">{warranty?.producto.descripcion}</p>
+                <p className="label col-span-3">SKU:</p> <p className="value col-span-3">{warranty?.producto.sku}</p>
+                <p className="label col-span-3">Equipo enciende:</p> <p className="value col-span-3">{warranty?.producto.enciende ? "Sí" : "No"}</p>
+                <p className="label col-span-3">Contraseña:</p> <p className="value col-span-3">{warranty?.producto.contrasena}</p>
+                <p className="label col-span-3">Se puede formatear:</p> <p className="value col-span-3">{warranty?.producto.puede_formatear ? "Sí" : "No"}</p>
+            </section>
+            <section className="warranty-section">
+                <p className="label col-span-3">Procesador:</p> <p className="value col-span-3">{warranty?.producto.cpu}</p>
+                <p className="label col-span-3">Tarjeta de video:</p> <p className="value col-span-3">{warranty?.producto.gpu}</p>
+                <p className="label col-span-3">Memoria RAM:</p> <p className="value col-span-3">{warranty?.producto.ram.cantidad}</p>
+                <p className="label col-span-3">Módulos:</p> <p className="value col-span-3">{warranty?.producto.ram.modulos}</p>
+
                 {warranty?.producto.almacenamiento.map((el, i) => (
-                    <div className="flex row" key={i}>
-                        <div className="flex-1 flex column"><p className="label">Almacenamiento:</p> <p className="value">{el.cantidad}</p></div>
-                        <div className="flex-1 flex column"><p className="label">Tipo:</p> <p className="value">{el.tipo}</p></div>
+                    <div key={i} className="grid grid-cols-12 gap-px col-span-12">
+                        <p className="label col-span-3">Almacenamiento:</p> <p className="value col-span-3">{el.cantidad}</p>
+                        <p className="label col-span-3">Tipo:</p> <p className="value col-span-3">{el.tipo}</p>
                     </div>
                 ))}
             </section>
-            <section>
-                <div className="flex-1 flex row"><p className="label">Tapas:</p> <p className="value">En caso de ser equipo de escritorio</p></div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Tapa izquierda:</p> <p className="value">{warranty?.producto.tapa_izquierda.material}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Tapa derecha:</p> <p className="value">{warranty?.producto.tapa_derecha.material}</p></div>
-                </div>
+            <section className="warranty-section">
+                <p className="label col-span-4">Tapas:</p> <p className="value col-span-8">En caso de ser equipo de escritorio</p>
+                <p className="label col-span-3">Tapa izquierda:</p> <p className="value col-span-3">{warranty?.producto.tapa_izquierda.material}</p>
+                <p className="label col-span-3">Tapa derecha:</p> <p className="value col-span-3">{warranty?.producto.tapa_derecha.material}</p>
             </section>
-            <section>
-                <div className="flex-1 flex row"><p className="label">Fuente de Poder:</p> <p className="value">{warranty?.producto.psu.descripcion}</p></div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Cantidad de watts:</p> <p className="value">{warranty?.producto.psu.watts}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Viene con cable:</p> <p className="value">{warranty?.producto.psu.cable ? "Sí" : "No"}</p></div>
-                </div>
+            <section className="warranty-section">
+                <p className="label col-span-4">Fuente de Poder:</p> <p className="value col-span-8">{warranty?.producto.psu.descripcion}</p>
+                <p className="label col-span-3">Cantidad de watts:</p> <p className="value col-span-3">{warranty?.producto.psu.watts}</p>
+                <p className="label col-span-3">Viene con cable:</p> <p className="value col-span-3">{warranty?.producto.psu.cable ? "Sí" : "No"}</p>
             </section>
-            <section>
-                <div className="flex-1 flex row"><p className="label">Cargador:</p> <p className="value">En caso de ser Laptop</p></div>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Viene incluido:</p> <p className="value">{warranty?.producto.cargador.incluido ? "Sí" : "No"}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Capacidad:</p> <p className="value">{warranty?.producto.cargador.capacidad}</p></div>
-                    <div className="flex-1 flex column"><p className="label">Notas:</p> <p className="value">{warranty?.producto.cargador.notas}</p></div>
-
-                </div>
+            <section className="warranty-section">
+                <p className="label col-span-4">Cargador:</p> <p className="value col-span-8">En caso de ser Laptop</p>
+                <p className="label col-span-2">Viene incluido:</p> <p className="value col-span-2">{warranty?.producto.cargador.incluido ? "Sí" : "No"}</p>
+                <p className="label col-span-2">Capacidad:</p> <p className="value col-span-2">{warranty?.producto.cargador.capacidad}</p>
+                <p className="label col-span-2">Notas:</p> <p className="value col-span-2">{warranty?.producto.cargador.notas}</p>
+            </section >
+            <section className="warranty-section grid-rows-8">
+                <p className="label col-span-4">Estado del Equipo</p> <p className="value col-span-8"></p>
+                <p className="label col-span-4">Número de serie:</p> <p className="value col-span-8">{warranty?.producto.serie}</p>
+                <p className="label col-span-12">Estado físico y Observaciones:</p> <p className="value col-span-12 row-span-2">{warranty?.producto.estado_fisico}</p>
+                <p className="label col-span-12">Detalle del problema o trabajo a realizar:</p> <p className="value col-span-12 row-span-2">{warranty?.producto.problema}</p>
             </section>
-            <section>
-                <div className="flex-1 flex row"><p className="label">Estado del Equipo</p> <p className="value"></p></div>
-                <div className="flex-1 flex row"><p className="label">Número de serie:</p> <p className="value">{warranty?.producto.serie}</p></div>
-                <div className="flex-1 flex flex-col row"><p className="label">Estado físico y Observaciones:</p> <p className="value">{warranty?.producto.estado_fisico}</p></div>
-                <div className="flex-1 flex flex-col row"><p className="label">Detalle del problema o trabajo a realizar:</p> <p className="value">{warranty?.producto.problema}</p></div>
+            <section className="warranty-section">
+                <p className="label col-span-3">Firma del Cliente</p> <p className="value col-span-3"></p>
+                <p className="label col-span-3">Firma de Recepción</p> <p className="value col-span-3"></p>
             </section>
-            <section>
-                <div className="flex row">
-                    <div className="flex-1 flex column"><p className="label">Firma del Cliente</p> <p className="value"></p></div>
-                    <div className="flex-1 flex column"><p className="label">Firma de Recepción</p> <p className="value"></p></div>
-                </div>
-            </section>
-        </div>
+        </div >
     )
 }
