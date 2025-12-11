f0f62da6bf0e857bbbeece739ce36f29fb52bc7b
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-11 16:20:02 -0600

    UPD: clerk proxy

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,30 @@
 export const runtime = "edge";
-import { clerkMiddleware, createRouteMatcher, } from '@clerk/nextjs/server';
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
     "/sign-in(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
+
+    // Redirect "/" → "/sign-in"
+    if (req.nextUrl.pathname === "/") {
+        return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    // Protect everything except /sign-in
     if (!isPublicRoute(req)) {
         await auth.protect();
     }
 });
 
-
-
-//
-// Config de matcher recomendada por Clerk
-//
 export const config = {
     matcher: [
-        // Skip internals + static files
         "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-        // API siempre con middleware
         "/(api|trpc)(.*)",
+        "/dashboard/:path*",
+        "/"
     ],
 };
