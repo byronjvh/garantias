11bf790f00614de26cd1daffd4553664e2e2201d
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-11 14:33:50 -0600

    UPD: simplify loading style

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -11,12 +11,7 @@ export default function PrimaryButton({ text, onClick, loading }: Props) {
         <button onClick={onClick} className="bg-accent cursor-pointer px-4 py-2 text-sm font-bold text-white hover:brightness-110 ease-out transition-all duration-200 rounded relative">
             <p className={`inline ${loading ? "opacity-0" : "opacity-100"}`}>{text}</p>
             <div className={`loading-effect flex flex-nowrap absolute left-1/2 top-1/2 -translate-1/2 ${loading ? "inline" : "hidden"}`}>
-                Cargando
-                <div className="dots ml-0.5 inline">
-                    <span className="dot-1">.</span>
-                    <span className="dot-2">.</span>
-                    <span className="dot-3">.</span>
-                </div>
+                Cargando...
             </div>
         </button>
     )
