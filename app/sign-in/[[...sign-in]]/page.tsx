e8923704fac27c97425860e2ef5f82969ab0499c
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-11 16:20:57 -0600

    UPD: signin page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn withSignUp={true} forceRedirectUrl="/dashboard" />
+    return (
+        <div className="w-full max-w-md flex flex-col items-center gap-2 pt-4 px-2 mx-auto">
+            <img className="w-full" src="/Tukomer.png" alt="" />
+            <SignIn withSignUp={true} forceRedirectUrl="/dashboard" />
+        </div>
+    )
 }
