275230365f88f870f22a0d6ffd8c828e23dc96e0
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-11 14:33:13 -0600

    add: auth validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-p dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Link href="/dashboard">Ingresar</Link>
-      </main>
-    </div>
-  );
+export default async function HomePage() {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/sign-in");
+
+  redirect("/dashboard");
 }
+
