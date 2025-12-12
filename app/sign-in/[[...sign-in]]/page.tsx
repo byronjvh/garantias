3ce71b973013bfb049d2aeb872538aa18911f001
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-12 16:57:35 -0600

    add: db logic for create users

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div className="w-full max-w-md flex flex-col items-center gap-2 pt-4 px-2 mx-auto">
             <img className="w-full" src="/Tukomer.png" alt="" />
-            <SignIn withSignUp={true} forceRedirectUrl="/dashboard" />
+            <SignIn withSignUp={true} />
         </div>
     )
 }
