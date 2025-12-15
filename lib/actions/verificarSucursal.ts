9284c89b0616406dba7437ada89b541f9ef65bcc
Byron Vargas Hernandez <byronjvh02@gmail.com>
2025-12-15 17:01:01 -0600

    add: action to verify user sucursal

---
+++ lib/actions/verificarSucursal.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { prisma } from "@/lib/prisma";
+
+
+
+export async function verificarSucursal(id: string) {
+
+    const usuario = await prisma.usuario.findUnique({
+        where: { clerkId: id },
+        select: { sucursalId: true },
+    });
+
+    console.log("funciona?")
+
+    return {
+        success: true,
+        tieneSucursal: !!usuario?.sucursalId,
+        sucursalId: usuario?.sucursalId,
+    };
+}
