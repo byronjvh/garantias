d49f2cc6d107a034ea0913160d4dd83114f08b79
Byron Vargas <byronjvh02@gmail.com>
2025-12-14 21:48:04 -0600

    UPD: improve middleware logic

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,26 @@
-export const runtime = "nodejs"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
+// Definimos públicas
 const isPublicRoute = createRouteMatcher([
     "/sign-in(.*)",
-]);
+    "/api/webhooks/clerk(.*)"
+])
+
 
 export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) await auth.protect()
-});
+    const { userId, redirectToSignIn } = await auth()
+    if (!isPublicRoute(req) && !userId) {
+        return redirectToSignIn()
+    }
+
+})
 
 export const config = {
     matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        // Este es el matcher que da Clerk en su ejemplo oficial
         "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // Always run for API routes
         "/(api|trpc)(.*)",
-        "/dashboard/:path*",
-        "/"
     ],
-};
+}
